79f05a629e8e07dccf66d76a69e11c37076f1f81
Marius Magureanu <mariusmagureanu@live.dk>
2026-01-19 20:36:05 +0100

    Added some extra goodies

---
+++ nvim/lua/hecker/plugins/misc.lua
@@ -0,0 +1,56 @@
+return {
+
+	{ "chriskempson/base16-vim", lazy = false },
+
+	{ "morhetz/gruvbox", lazy = false },
+
+	{ "HiPhish/rainbow-delimiters.nvim" },
+
+	{ --* Makes hover window prettier *--
+		"Fildo7525/pretty_hover",
+		opts = true,
+		keys = {
+			{
+				"K",
+				function()
+					require("pretty_hover").hover()
+				end,
+				desc = "Hover",
+			},
+		},
+	},
+
+	{ --* Beautiful markdown previewer *--
+		"OXY2DEV/markview.nvim",
+		ft = "markdown",
+		opts = { -- Do not render text on the line you are on
+			preview = {
+				hybrid_modes = { "n" },
+				callbacks = {
+					on_enable = function(_, win)
+						vim.wo[win].conceallevel = 2
+						vim.wo[win].concealcursor = "c"
+					end,
+				},
+			},
+		},
+	},
+
+	{ --* toggle booleans, increment date & months via <C-a> & <C-x> *--
+		"nat-418/boole.nvim",
+		keys = { "<C-a>", "<C-x>" },
+		opts = { mappings = { increment = "<C-a>", decrement = "<C-x>" } },
+	},
+
+	{
+		"preservim/tagbar",
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "*",
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({})
+		end,
+	},
+}
