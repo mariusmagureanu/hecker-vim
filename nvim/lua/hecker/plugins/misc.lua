c8a96585c1511e8d93e0590dc39c11c4eee49cfb
Marius Magureanu <marius.magureanu@schibsted.com>
2026-01-20 11:23:07 +0100

    Rely on snacks even more

--- nvim/lua/hecker/plugins/misc.lua
+++ nvim/lua/hecker/plugins/misc.lua
@@ -1,56 +1,68 @@
 return {
 
-	{ "chriskempson/base16-vim", lazy = false },
+  { "chriskempson/base16-vim",        lazy = false },
 
-	{ "morhetz/gruvbox", lazy = false },
+  { "morhetz/gruvbox",                lazy = false },
 
-	{ "HiPhish/rainbow-delimiters.nvim" },
+  { "HiPhish/rainbow-delimiters.nvim" },
 
-	{ --* Makes hover window prettier *--
-		"Fildo7525/pretty_hover",
-		opts = true,
-		keys = {
-			{
-				"K",
-				function()
-					require("pretty_hover").hover()
-				end,
-				desc = "Hover",
-			},
-		},
-	},
+  { --* Makes hover window prettier *--
+    "Fildo7525/pretty_hover",
+    opts = true,
+    keys = {
+      {
+        "K",
+        function()
+          require("pretty_hover").hover()
+        end,
+        desc = "Hover",
+      },
+    },
+  },
 
-	{ --* Beautiful markdown previewer *--
-		"OXY2DEV/markview.nvim",
-		ft = "markdown",
-		opts = { -- Do not render text on the line you are on
-			preview = {
-				hybrid_modes = { "n" },
-				callbacks = {
-					on_enable = function(_, win)
-						vim.wo[win].conceallevel = 2
-						vim.wo[win].concealcursor = "c"
-					end,
-				},
-			},
-		},
-	},
+  { --* Beautiful markdown previewer *--
+    "OXY2DEV/markview.nvim",
+    ft = "markdown",
+    opts = { -- Do not render text on the line you are on
+      preview = {
+        hybrid_modes = { "n" },
+        callbacks = {
+          on_enable = function(_, win)
+            vim.wo[win].conceallevel = 2
+            vim.wo[win].concealcursor = "c"
+          end,
+        },
+      },
+    },
+  },
 
-	{ --* toggle booleans, increment date & months via <C-a> & <C-x> *--
-		"nat-418/boole.nvim",
-		keys = { "<C-a>", "<C-x>" },
-		opts = { mappings = { increment = "<C-a>", decrement = "<C-x>" } },
-	},
+  { --* toggle booleans, increment date & months via <C-a> & <C-x> *--
+    "nat-418/boole.nvim",
+    keys = { "<C-a>", "<C-x>" },
+    opts = { mappings = { increment = "<C-a>", decrement = "<C-x>" } },
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    lazy = false,
+    config = function()
+      require("gitsigns").setup()
+    end,
+  },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true,
+  },
 
-	{
-		"preservim/tagbar",
-	},
-	{
-		"kylechui/nvim-surround",
-		version = "*",
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup({})
-		end,
-	},
+  {
+    "preservim/tagbar",
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({})
+    end,
+  },
 }
